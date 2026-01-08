d66a53bffe6424f76b52fcf05b8e5f7bfc94bfe6
Jiyabisht <bishtjiya.2005@gmail.com>
2026-01-08 17:24:25 +0530

    second largest element in an array
    Updated logic to handle cases with negative numbers correctly and improved variable initialization.

--- A2.cpp
+++ A2.cpp
@@ -2,14 +2,46 @@
 #include<vector>
 using namespace std;
 int largest(vector<int>&arr,int n){
-    int max=arr[0],max2=arr[0];
+    int max=arr[0],max2=-1; //if no negative no. in an array
+    for(int i=0;i<n;i++){        //5
+        if(arr[i]>max){          //-10 -20 -5 -30 -15
+            max=arr[i];          //Correct second largest → -10
+        }                        //Your output → -1 ❌ (not even in array)
+    }
     for(int i=0;i<n;i++){
-        if(arr[i]>max){
+        if(arr[i]>max2 && arr[i]!=max){
+            max2=arr[i];
+        }
+    }
+    return max2;
+}
+int main(){
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"enter all elements";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"second largest is "<<largest(arr,n);
+    return 0;
+}
+
+
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+int largest(vector<int>&arr,int n){
+    int max=arr[0],max2=INT_MIN;  //if negative no. is there in an array
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){           //INT_MIN is a constant that represents the smallest possible value an int data type can store in C++.
             max=arr[i];
         }
     }
     for(int i=0;i<n;i++){
-        if(arr[i]>max2 && arr[i]!=max){
+        if(arr[i]>max2 && arr[i]<max){
             max2=arr[i];
         }
     }
