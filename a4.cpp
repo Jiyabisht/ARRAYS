2e80e41df2590179d116ca67de72ff1eeb10f287
Jiyabisht <bishtjiya.2005@gmail.com>
2026-01-12 08:30:43 +0530

    Remove Duplicates from an array
    Implemented a two-pointer approach to remove duplicates from a sorted array.

--- a4.cpp
+++ a4.cpp
@@ -38,3 +38,18 @@ int main(){
 
 
 //without stack we can use two pointer approach
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0) return 0;
+        int i=0;
+        for(int j=1;j<n;j++){
+            if(nums[i]!=nums[j]){
+                nums[i+1]=nums[j];
+                i++;
+            }
+        }
+    return (i+1);                 //we have to return only the count
+    }
+};
