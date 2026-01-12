0796c702b5671bff965e87342c0fc56fe58f1c32
Jiyabisht <bishtjiya.2005@gmail.com>
2026-01-13 00:24:25 +0530

    Left rotate an array by 1 place

--- a5.cpp
+++ a5.cpp
@@ -2,26 +2,32 @@
 #include<vector>
 #include<stack>
 using namespace std;
-int duplicates(vector<int>&arr, int n){
+int duplicates(vector<int>&arr, int n, int k){
+    k=k%n;
+    while(k!=0){
     int temp=arr[0];
     for(int i=1;i<n;i++){
         arr[i-1]=arr[i];
     }
     arr[n-1]=temp;
+    k--;
+    }
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
     return 0;
 }
 int main(){
-    int n;
+    int n,k;
     cout<<"enter n";
     cin>>n;
+    cout<<"enter k";
+    cin>>k;
     vector<int>arr(n);
     cout<<"enter elements";
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    duplicates(arr,n);
+    duplicates(arr,n,k);
     return 0;
 }
