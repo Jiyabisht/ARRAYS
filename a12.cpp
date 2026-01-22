0521f63b5a586b8cc2c866e5ef58365894ca268a
Jiya Bisht <bishtjiya.2005@gmail.com>
2026-01-23 00:43:44 +0530

    Create a12.cpp

---
+++ a12.cpp
@@ -0,0 +1,18 @@
+//bruteforce code
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>res(n);
+        for(int i=0;i<n;i++){
+            int pro=1;
+            for(int j=0;j<n;j++){
+                if(j!=i){
+                    pro=pro*nums[j];
+                }
+            }
+            res[i]=pro;
+        }
+        return res;
+    }
+};
