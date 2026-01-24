45f83c4c95b8f61b1cfacf061c063774d9a03bdd
Jiya Bisht <bishtjiya.2005@gmail.com>
2026-01-25 02:32:14 +0530

    Two sum

--- a15.cpp
+++ a15.cpp
@@ -27,3 +27,18 @@ int main(){
 }
 
 //optimal
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;      //<key,value> i.e. <element,index>
+
+        for(int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];       // if target is 4 arr is [1,2,3] then 4-1 is 3 ,next heck for 3 in map is present return index else add it to map
+            if(mp.find(need) != mp.end()) {    //mp.find(need):Searches for key = need in the hashmap     Returns:an iterator to that key if it exists
+                return {mp[need], i};          //mp.end():Iterator that points just after the last element     Used as a marker for “not found”
+            }
+            mp[nums[i]] = i;                   //if not found then add that element(new) to map
+        }
+        return {};
+    }
+};
