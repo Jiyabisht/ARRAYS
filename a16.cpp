2c2c0420ff6ca4edaf913eb7dd0194074eef7b03
Jiya Bisht <bishtjiya.2005@gmail.com>
2026-02-02 23:13:19 +0530

    Single no.

--- a16.cpp
+++ a16.cpp
@@ -19,7 +19,7 @@ public:
     int singleNumber(vector<int>& nums) {
         unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+            mp[nums[i]]++;  //automatically count is 0 then it increases
         }
         for(int i = 0; i < nums.size(); i++) {
             if(mp[nums[i]] == 1) {
