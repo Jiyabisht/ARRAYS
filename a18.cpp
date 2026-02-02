1440061c02651b11fe7ee34f664cb0a27f82706c
Jiya Bisht <bishtjiya.2005@gmail.com>
2026-02-03 00:10:22 +0530

    Number of Students Unable to Eat Lunch

---
+++ a18.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int c1=0,c0=0;
+        for(int i=0;i<students.size();i++){
+            if(students[i]==0){
+                c0++;
+            }else{
+                c1++;
+            }
+        }
+        for(int i=0;i<sandwiches.size();i++){
+            if(sandwiches[i]==0){
+                if(c0==0) break;
+                c0--;
+            }else{
+                if(c1==0) break;
+                c1--;
+            }
+        }
+        int sum= c0+c1;
+        return sum;
+    }
+};
