ab5bba20e5b501e5a08abd16718d5abcb77aafe3
Jiya Bisht <bishtjiya.2005@gmail.com>
2026-02-05 01:03:12 +0530

    buy two chocolates

--- a17.cpp
+++ a17.cpp
@@ -14,7 +14,7 @@ public:
     }
 };
 
-
+//OR
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
@@ -27,3 +27,37 @@ public:
             return money;
     }
 };
+//Time complexity=O(nlog n)
+
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int min1 = INT_MAX, min2 = INT_MAX;
+
+        for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] < min1) {
+                min2 = min1;
+                min1 = prices[i];
+            }
+            else if (prices[i] < min2) {
+                min2 = prices[i];
+            }
+        }
+
+        int cost = min1 + min2;
+
+        if (cost <= money)
+            return money - cost;
+
+        return money;
+    }
+};
+//Complexity=O(n)
+prices = [5, 2, 4, 1]
+| i | prices[i] | min1 | min2 |
+| - | --------- | ---- | ---- |
+| 0 | 5         | 5    | INF  |
+| 1 | 2         | 2    | 5    |
+| 2 | 4         | 2    | 4    |
+| 3 | 1         | 1    | 2    |
+
