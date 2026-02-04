14a0bb139c58ff02cac9554401c953bfe7c14263
Jiya Bisht <bishtjiya.2005@gmail.com>
2026-02-05 02:19:11 +0530

    Power of 2

---
+++ a19.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        while(n>1){
+            if(n%2!=0){
+                return false;
+            }
+            n=n/2;
+        }
+        return true;
+    }
+};
+
+//complexity=O(log n)
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return n>0 && (n & (n-1))==0;
+    }
+};
+//Complexity=O(1)
